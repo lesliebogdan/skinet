f383b52675e1f57f7b0c767a9595fc2dd41fb167
leslie bogdan <leslie@lesliebogdan.com>
2023-07-18 12:40:35 +1000

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
@@ -29,5 +32,21 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+// DB Build from migration
+
+using var scope = app.Services.CreateScope();
+// dispose method will be called once we are finished with this
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try {
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex) {
+    logger.LogError(ex,"An error occured during migration");
+}
 
 app.Run();
