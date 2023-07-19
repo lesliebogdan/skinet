f54cd0ec87d1b984a5095d09a0c8d1601fb7be7b
leslie bogdan <leslie@lesliebogdan.com>
2023-07-19 17:26:00 +1000

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -28,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
