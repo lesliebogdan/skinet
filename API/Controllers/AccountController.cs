15cfabf831b6827c78ab4be51aada3241b05f463
leslie bogdan <leslie@lesliebogdan.com>
2023-09-14 12:12:20 +1000

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -100,6 +100,11 @@ public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 [HttpPost("register")]
 public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
 {
+    if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+    {
+        return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
+    }
+    
     var user = new AppUser{
         DisplayName = registerDto.DisplayName,
         Email = registerDto.Email,
--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddControllers();
 // we have moved our Application Services => Extensions/ApplicationServicesExtensions
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 
 var app = builder.Build();
 
@@ -34,8 +35,9 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+app.UseSwaggerDocumentation();
+
 //}
 
 app.UseStaticFiles();
